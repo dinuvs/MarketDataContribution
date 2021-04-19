3ac5710a2cc47c7cc09e22e6cf41ffc350ceab88
Dinesh VS <dinu.vs@gmail.com>
2021-04-19 07:36:51 +0100

    add marketdata controller get list tests

---
+++ MarketDataContribution.Main.Tests/ControllerTests/MarketDataControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketDataContribution.Main.Tests.ControllerTests
+{
+    [TestClass]
+    public class MarketDataControllerTests
+    {
+        [TestMethod]
+        public void GetAllMarketDataListReturnsNonZeroList()
+        {
+
+            //var result=
+           // Assert.IsTrue(result.Count>0)
+        }
+
+        [TestMethod]
+        public void GetAllMarketDataListReturnsCountZero()
+        {
+
+            //var result=
+            // Assert.IsTrue(result.Count==0)
+        }
+
+    }
+}
